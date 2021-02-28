1ab26ff6c22b5e228874deec55828c6b931b9ecc
tejasajo <teja.sajovic@siol.net>
2021-02-28 18:48:28 +0100

    Create Gravitacija.java

---
+++ ois/v1/ois/v1/Gravitacija.java
@@ -0,0 +1,23 @@
+public class Gravitacija{
+    public static void main(String[] args){
+    System.out.print("OIS je zakon!");
+    }
+}
+import java.util.Scanner;
+import java.util.*;
+public class Pospesek {
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+        double visina = scan.nextDouble();
+
+        double a = Math.pow(10, -11);
+        double b = Math.pow(10, 24);
+        double c = Math.pow(10, 6);
+
+        double pospesek = (6.674 * a * 5.972 * b) / ((6.371 * c + visina) * (6.371 * c + visina));
+
+        System.out.println(pospesek);
+    }
+}
